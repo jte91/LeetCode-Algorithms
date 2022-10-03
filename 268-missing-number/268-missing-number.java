1f561f33510db5c9268d93234ca690f318a3a77f
Jay Edwards <101276265+jte91@users.noreply.github.com>
2022-10-03 17:20:49 -0500

    Time: 6 ms (28.88%), Space: 43 MB (92.44%) - LeetHub

---
+++ 268-missing-number/268-missing-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int missingNumber(int[] nums)
+    {
+        Set<Integer> set = new HashSet<>();
+        for(int i=0; i<nums.length; i++)
+        {
+            set.add(nums[i]);
+        }
+        for(int i=0;i<nums.length + 1;i++)
+        {
+            if(!set.contains(i))
+            {
+                return i;
+            }
+        }
+        return 1;
+    }
+}
